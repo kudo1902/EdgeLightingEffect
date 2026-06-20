f852c5538bb764bb69ccc424b5e66c347202a6c1
Nguyen Truong <nhattruong19021997@gmail.com>
2026-06-20 10:56:14 +0700

    add constant defines

---
+++ lib/include/util/constants.h
@@ -0,0 +1,9 @@
+#ifndef _EDGE_LIGHTING_CONSTANTS_H_
+#define _EDGE_LIGHTING_CONSTANTS_H_
+
+namespace EdgeLighting
+{
+    constexpr float PI = 3.14159265358979323846f;
+} // namespace EdgeLighting
+
+#endif // _EDGE_LIGHTING_CONSTANTS_H_
